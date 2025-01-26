aebf1fa0efaa51e36a2023d81069ae9afaa56b8b
Mikhail Mefodyev <hg-4ybak@mail.ru>
2025-01-26 23:18:12 +0300

    feat: add docker-compose and run script

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Clone repositories to store proto contracts locally.
+# That is needed for kafka-ui.
+
+if [[ ! -d "task-service" ]]; then
+	git clone git@github.com:itmo-bebriki/task-service.git
+fi
+
+if [[ ! -d "board-service" ]]; then
+	git clone git@github.com:itmo-bebriki/board-service.git
+fi
+
+if [[ ! -d "analytics-service" ]]; then
+	git clone git@github.com:itmo-bebriki/analytics-service.git
+fi
+
+docker-compose up -d
+
+# Sleep 5 seconds and then restart all services.
+# That is an obligatory operation, because without that
+# services are bugged (they cannot load schema or something like this).
+# A simple restart heals this behaviour.
+sleep 5s
+
+docker restart task-service
+docker restart board-service
+docker restart analytics-service
